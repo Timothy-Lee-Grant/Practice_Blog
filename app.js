53cf75ffeeb55ee54313c7b5679e39d530b3569a
Timothy <timothyleegrant1995@gmail.com>
2024-08-07 21:18:28 -0700

    Added middleware function that responds to /test_api

--- app.js
+++ app.js
@@ -6,8 +6,14 @@ const express = require('express');
 const app  = express();
 const PORT = 8080 || process.env.PORT;
 
+//my own experiment to see if I could create a costum api endpdoint instead of the app.get('', (vars)=>{funct})
+app.get('/test_api', (req, res) => {
+    res.send("Is this the one that you wanted?")
+})
+
 app.get('', (req, res)=>{
-    res.send("Hellow world");
+    console.log("I just ran the app.get function");
+    res.send("Hello world. I got it!!!");
 })
 
 app.listen(PORT, ()=>{
