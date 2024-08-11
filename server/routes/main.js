5c0104077a804c1d2e72581a8717f1e1c1c218ac
Timothy <timothyleegrant1995@gmail.com>
2024-08-11 14:54:32 -0700

    Added main.ejs and index.ejs into the layout file. This will introduce html functionality into the project.

--- server/routes/main.js
+++ server/routes/main.js
@@ -2,6 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 //middleware functions that service api requests
+router.get('', (req, res) => {
+    res.render('index');
+})
+/*
 router.get('/test_api', (req, res) => {
     res.send("Is this the one that you wanted?")
 })
@@ -10,6 +14,7 @@ router.get('', (req, res)=>{
     console.log("I just ran the app.get function");
     res.send("Hello world. I got it!!!");
 })
+*/
 
 //Watched a video about modules. It said that each file is called a 'module' and the functions/variables
 //defined in that file are contained in that module. Performing a module.export will allow access into this
