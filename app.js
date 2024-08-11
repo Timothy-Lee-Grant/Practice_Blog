184dceb248b23eacde3395789febc42bd4f11c96
Timothy <timothyleegrant1995@gmail.com>
2024-08-10 18:24:32 -0700

    Successfully relocated the two api endpoints from the app.js file to the main.js file

--- app.js
+++ app.js
@@ -1,12 +1,30 @@
 require('dotenv').config();
 
+//express is a middlewear library that allows for easy handling of 
 const express = require('express');
+const expressLayout = require('express-ejs-layouts');
 
 //Investigate - what does this 'express' actual do?
 const app  = express();
 const PORT = 8080 || process.env.PORT;
 
+
+//trying to get this to work
+
+app.use(express.static('public'));
+
+app.use(expressLayout);
+app.set('layout', './layouts/main');
+app.set('view engine', 'ejs');
+
+app.use('/', require('./server/routes/main'));
+
+
+//end Trying to get this to work.
+
+
 //my own experiment to see if I could create a costum api endpdoint instead of the app.get('', (vars)=>{funct})
+/*
 app.get('/test_api', (req, res) => {
     res.send("Is this the one that you wanted?")
 })
@@ -15,6 +33,7 @@ app.get('', (req, res)=>{
     console.log("I just ran the app.get function");
     res.send("Hello world. I got it!!!");
 })
+*/
 
 app.listen(PORT, ()=>{
     console.log(`App listening on port ${PORT}`);
