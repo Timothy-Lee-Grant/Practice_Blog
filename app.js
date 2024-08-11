ca92e0f1861da0a3ea100b54b76ce4df6628896d
Timothy <timothyleegrant1995@gmail.com>
2024-08-11 14:45:49 -0700

    added comments for self reference and understanding. Will need to revise comments once I learn and familiarize myself with concepts more.

--- app.js
+++ app.js
@@ -4,37 +4,24 @@ require('dotenv').config();
 const express = require('express');
 const expressLayout = require('express-ejs-layouts');
 
-//Investigate - what does this 'express' actual do?
+//Express is a service that allows handling of HTTP requests
 const app  = express();
+//PORT will be set to 8080 or the PORT specified in the process.env
 const PORT = 8080 || process.env.PORT;
 
-
-//trying to get this to work
-
+//This function will tell app (express) to look in 'public' as the folder location, to find static files (static files are those css, html, JS, ect) 
+//This allows the client to access the files within 'public' and because the files are not dynamic, 
 app.use(express.static('public'));
 
+//layouts are a way to Embedded Java Script files 
+//as of right now, not sure what that actually means or what it does. 
 app.use(expressLayout);
 app.set('layout', './layouts/main');
 app.set('view engine', 'ejs');
 
 app.use('/', require('./server/routes/main'));
 
-
-//end Trying to get this to work.
-
-
-//my own experiment to see if I could create a costum api endpdoint instead of the app.get('', (vars)=>{funct})
-/*
-app.get('/test_api', (req, res) => {
-    res.send("Is this the one that you wanted?")
-})
-
-app.get('', (req, res)=>{
-    console.log("I just ran the app.get function");
-    res.send("Hello world. I got it!!!");
-})
-*/
-
+//This will start the server (express) to perform the listen and will wait for HTTP requests that come to it over the port.
 app.listen(PORT, ()=>{
     console.log(`App listening on port ${PORT}`);
 })
