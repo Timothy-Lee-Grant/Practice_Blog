cf3db463a793cfc63a14923af4d22c58f9efd5a5
Timothy <timothyleegrant1995@gmail.com>
2024-09-02 14:11:13 -0700

    Added a post.js file that will create a new schema to the mongodb database.

--- server/routes/main.js
+++ server/routes/main.js
@@ -1,22 +1,47 @@
 const express = require('express');
 const router = express.Router();
+const Post = require('../models/Post');
 
 //middleware functions that service api requests
-router.get('', (req, res) => {
+router.get('', async (req, res) => {
     const locals = {
         title: "NodeJs Blog",
         description: "A simple blog that I am making to practice my skills with express, nodejs, and mongodb."
     }
 
-    //Notice how I am able to pass in two arguments. This will go to the main.ejs template, and then inside of the main.ejs file I use those <%= xxxx %> 
-    //to be able to place in variables into that specific location in order to costomize it. In this instance I can do <%= locals.description %> to 
-    //access the description field.
-    res.render('index', {locals});
+    try {
+        const data = await Post.find();
+        //Notice how I am able to pass in two arguments. This will go to the main.ejs template, and then inside of the main.ejs file I use those <%= xxxx %> 
+        //to be able to place in variables into that specific location in order to costomize it. In this instance I can do <%= locals.description %> to 
+        //access the description field.
+        res.render('index', {locals, data});
+    } catch (error) {
+        console.log(error);
+    }
+
+    
 })
 
 router.get('', (req, res) => {
     res.render('index');
 })
+
+
+function insertPostData(){
+    Post.insertMany([
+        {
+            title: "A blog title that I just created",
+            body: "This is the body of the blog post that I want to create. "
+        },
+        {
+            title: "Another Blog title",
+            body: "Another body to another blog."
+        },
+    ])
+}
+
+//insertPostData();
+
 
 //seems as if when I do this render, I am able to put the 'render' as an argument. This will tell the '/about' api (this api) to 
 //utilize the about.ejs file. So previously in the app.js, we told express to set 'layout' to be directed to the ./views/layouts/main.ejs (actually we did not need to set the views directory, even though the file is in views)
